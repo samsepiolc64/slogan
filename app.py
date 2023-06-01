0816955cbbacc772c3c6f2102af4f04ed32db662
samsepiolc64 <mjarmuzek@wp.pl>
2023-06-01 10:50:46 +0200

    add env

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for, Response, flash, session
+from os import getenv
+import openai
+import os
 
-
+openai.api_key  = getenv('API_KEY')
 app = Flask(__name__)
 
 @app.route('/')
 def start():
-    txt = "slogan"
-    return render_template('index.html', txt=txt)
+    lamp_review = """
+    Odkryj multifunkcyjną maskę do twarzy Skin Ready z serii INGLOT PLAYINN i pozwól swojej skórze poczuć TĘ MOC! Moc głębokiego nawilżenia, ujędrnienia oraz uczucia wygładzonej, aksamitnej skóry. Wypróbuj produkt, który wyrównuje drobne zmarszczki oraz opóźnia proces starzenia się skóry, wspomagając odnowę komórkową naskórka i wzmacniając jego strukturę.
+    """
+    prompt = f"""
+    Napisz 5 haseł reklamowych dla tego produktu. \
+    Niech hasło będzie maksymalnie z 10 wyrazów.
+    Review text: '''{lamp_review}'''
+    """
+    response = get_completion(prompt)
+    return render_template('index.html', txt=response)
