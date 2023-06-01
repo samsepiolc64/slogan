04e1d07482516c8643139ff9e7a7fd5a799a1c0c
samsepiolc64 <mjarmuzek@wp.pl>
2023-06-01 10:59:19 +0200

    add get_completion function

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ import os
 openai.api_key  = getenv('API_KEY')
 app = Flask(__name__)
 
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
+
 @app.route('/')
 def start():
     lamp_review = """
